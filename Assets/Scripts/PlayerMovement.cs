26a37c0db91dd3ed59037ad20435f78bee2b5674
sandsocks <max.sandejas@gmail.com>
2025-12-06 00:42:30 +0800

    yay

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,35 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float MovementSpeed = 5f;
-    [SerializeField] public float skinWidth = 0.05f;
-    [SerializeField] GameObject player;
-    [SerializeField] float distances;
-    [SerializeField] LayerMask layerMask;
-    [SerializeField] public float radius;
+    [SerializeField] float MovementSpeed;
+
     void Update()
     {
-        Movement();
-    }
-
-    private void Movement()
-    {
         Vector3 moveVec = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        //player.transform.position += moveVec * MovementSpeed * Time.deltaTime;
         Vector3 move = moveVec * MovementSpeed * Time.deltaTime;
 
-        if (moveVec != Vector3.zero)
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, moveVec, out hit, move.magnitude + skinWidth))
-            {
-                float distanceToWall = hit.distance - skinWidth;
-                transform.position += moveVec * Mathf.Max(0, distanceToWall);
-            }
-            else
-            {
-                transform.position += move;
-            }
-        }
+        if (moveVec != Vector3.zero) 
+        transform.position += moveVec;
     }
+
 }
